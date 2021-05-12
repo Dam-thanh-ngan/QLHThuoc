162364398f52b0af029c6a7a9210040d3e6aa72e
NGAN DAM THANH <1821050429@student.humg.edu.vn>
2021-05-12 20:47:48 +0700

    Add project files.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace QLHieuThuoc.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<QLHieuThuoc.Models.QLHTDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(QLHieuThuoc.Models.QLHTDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
